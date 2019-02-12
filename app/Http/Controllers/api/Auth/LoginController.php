689d66654331928417156e6ad50d4ce4d36ade44
unknown <acterprime1@gmail.com>
2019-02-12 18:17:21 +0600

    login and register

---
+++ app/Http/Controllers/api/Auth/LoginController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+   
+    protected $auth;
+
+   
+    public function __construct(JWTAuth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        // $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return response()->json([
+            'success' => false,
+            'errors' => [
+                " You have been locked out"]
+            
+        ]);
+        }
+        $this->incrementLoginAttempts($request);
+
+
+        try {
+            if (!$token = $this->auth::attempt($request->only('email', 'password'))) {
+                return response()->json([
+                'success' => false,
+                'errors' => [
+                  email =>[
+                    "Invalid email address or password"
+                  ]
+                ]
+            ], 422);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'success' => false,
+                'errors' => [
+                  email =>[
+                    "Invalid email address or password"
+                  ]
+                ]
+            ], 422);
+        }
+
+        /*  if ($this->attemptLogin($request)) {
+              return $this->sendLoginResponse($request);
+          }*/
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        return response()->json([
+                'success' => true,
+                 'data' => $request->user(),
+                 'token' => $token
+            ], 200);
+
+
+        //return $this->sendFailedLoginResponse($request);
+    }
+}
